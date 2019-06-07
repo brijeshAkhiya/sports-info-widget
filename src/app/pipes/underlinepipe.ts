294e3906abb5de4663558892033d49adc2872265
Savan Rajkotiya <savan.r@yudiz.in>
2019-06-07 18:25:16 +0530

    Scorecard Yet to bet and did not bet, Info tab on match detail page (Lineups are pending for implementation)

--- src/app/pipes/underlinepipe.ts
+++ src/app/pipes/underlinepipe.ts
@@ -5,11 +5,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 
 export class StringUnder implements PipeTransform {
-
     transform(input: string): any {
-        if(input){
-        return input.replace(/_/g," ")
+        if (input) {
+            // return input.replace(/_/g, " ")
+            return input.split("_").map(s => s.charAt(0).toUpperCase() + s.slice(1)).join(" ")
         }
-
     }
 }
