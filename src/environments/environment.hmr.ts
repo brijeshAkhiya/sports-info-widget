ba07f92722a16c8c64ededad30d88662bbdb5720
Yudiz Solutions <doli@yudiz.com>
2019-06-17 19:15:53 +0530

    env hmr

--- src/environments/environment.hmr.ts
+++ src/environments/environment.hmr.ts
@@ -1,13 +1,13 @@
 export const environment = {
     production: false,
     hmr: true,
-    apiUrl: 'https://backend.sports.info',
+    apiUrl: 'https://sports.bhavin.cf',
     version: '/api/v1',
     socket: {
-        baseUrl: 'https://backend.sports.info',
+        baseUrl: 'https://sports.bhavin.cf',
         config: {}
     },
     s3Url: 'https://d1ldsx0apuyt84.cloudfront.net/',
     siteUrl: 'http://dev.sports.info/',
     mapsKey: 'AIzaSyAjnz5zvaRF5aMwMcsZ2-5nm43B9Hs3KhY'
-};
+  };
