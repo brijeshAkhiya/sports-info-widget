71ef14366a086f178a38a19ffa9bd9216f82ee2c
Yudiz Solutions <doli@yudiz.com>
2019-06-18 11:13:34 +0530

    Added new credentials for development server

--- src/environments/environment.hmr.ts
+++ src/environments/environment.hmr.ts
@@ -1,13 +1,13 @@
 export const environment = {
     production: false,
     hmr: true,
-    apiUrl: 'https://sports.bhavin.cf',
+    apiUrl : 'https://dev.backend.sports.info',
     version: '/api/v1',
     socket: {
-        baseUrl: 'https://sports.bhavin.cf',
-        config: {}
+      baseUrl: 'https://dev.backend.sports.info',
+      config: {}
     },
-    s3Url: 'https://d1ldsx0apuyt84.cloudfront.net/',
-    siteUrl: 'http://dev.sports.info',
-    mapsKey: 'AIzaSyAjnz5zvaRF5aMwMcsZ2-5nm43B9Hs3KhY'
+    s3Url : 'https://d1ldsx0apuyt84.cloudfront.net/',
+    siteUrl : 'https://sports.info/',
+    mapsKey : 'AIzaSyAjnz5zvaRF5aMwMcsZ2-5nm43B9Hs3KhY'
   };
