7e4322461dbf5ae9c842d6a0d29e0b9e6b5963af
Savan Rajkotiya <savan.r@yudiz.in>
2019-07-03 11:35:20 +0530

    changes added - routing linking

--- src/app/shared/widget/search/search.component.ts
+++ src/app/shared/widget/search/search.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Output, EventEmitter, HostListener, ElementRef, Input, ViewChild } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, HostListener, ElementRef, Input, ViewChild, Renderer2 } from '@angular/core';
+import { Router } from '@angular/router';
+import { SportsService } from "@providers/sports-service";
 
 @Component({
   selector: 'app-search',
@@ -6,9 +8,14 @@ import { Component, OnInit, Output, EventEmitter, HostListener, ElementRef, Inpu
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-
+  searchkey: string;
+  searchdata: any;
+  noresults: boolean;
   constructor(
     private el: ElementRef,
+    private router: Router,
+    private renderer2: Renderer2,
+    private sportsService: SportsService
     ) { }
   
   @ViewChild('searchBox') searchBox;
@@ -26,6 +33,52 @@ export class SearchComponent implements OnInit {
   }
   
   ngOnInit() {
+  }
+
+  valuechange($e){
+    if($e.keyCode != 8 && this.searchkey.length > 2){
+      this.search();
+    }
+    else if(this.searchkey == '')
+      this.searchdata = [];
+  }
+
+  //search close
+  closesearch() {
+    this.issearch = false;
+    this.searchkey = "";
+    this.renderer2.removeClass(document.body, "search-box-open");
+  }
+
+  //search api call
+  search() {
+    if (this.searchkey.trim()) {
+      let data = {
+        sSearch: this.searchkey,
+        nLimit: 5,
+        nStart: 0
+      };
+      this.searchdata = [];
+      this.noresults = false;
+      this.sportsService.getsearchresult(data).subscribe(res => {
+        if (res["data"].length != 0) {
+          this.searchdata = res["data"];
+        } else {
+          this.noresults = true;
+        }
+      });
+    }
+  }
+
+  blogList(){
+    this.issearch = false;
+    this.renderer2.removeClass(document.body, "search-box-open");
+    this.router.navigate(['search', this.searchkey],{ 
+      state: { data:  this.searchdata, sSearch : this.searchkey} 
+    });
+    this.searchkey = '';
+    this.searchdata = [];
+
   }
 
 }
