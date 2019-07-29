190431c098f11a679d3fe2465ad84e3707ff137c
Rajnee Yudiz <rajnee.m@yudiz.in>
2019-07-29 11:32:14 +0530

    Fixture slider
    Fixture slider responsive and change slide option

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -31,15 +31,6 @@ export class HomeComponent implements OnInit {
     responsive: {
       0: {
         items: 1
-      },
-      400: {
-        items: 2
-      },
-      740: {
-        items: 3
-      },
-      940: {
-        items: 4
       }
     },
     nav: true
