ffa6f5a1a996a85cdb47a8f8bd82b12a9678f594
kinnari <kinnari.k@yudiz.in>
2019-08-08 15:14:12 +0530

    table sorting fix

--- src/app/shared/widget/player-table/player-table.component.ts
+++ src/app/shared/widget/player-table/player-table.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit, Input, ViewChildren, Directive, Output,EventEmitter, QueryList, ElementRef, Renderer } from '@angular/core';
-
+import { Component, OnInit, Input, ElementRef, Renderer } from '@angular/core';
 import { CommonService } from '@providers/common-service';
 import { CricketService } from '@providers/cricket-service';
 import { KabaddiService } from '@app/modules/kabaddi/kabaddi.service';
-import { stringify } from '@angular/compiler/src/util';
-import { filter, findIndex } from 'rxjs/operators';
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
-import { THROW_IF_NOT_FOUND } from '@angular/core/src/di/injector';
+
 
 @Component({
   selector: 'app-player-table',
@@ -27,21 +23,10 @@ export class PlayerTableComponent implements OnInit {
   constructor(
     private commonService: CommonService,
     private cricketService: CricketService,
-    private kabaddiService: KabaddiService,
-    private el: ElementRef, 
-    private renderer: Renderer) {
-  }
+    private kabaddiService: KabaddiService)
+  {}
 
-  ngOnInit() 
-  {
-      this.renderer.listen(this.el.nativeElement, 'click', (event) => {
-      if(this.data && this.value)
-      {
-         let sortedData: any = this.sortArray();
-      }  
-      this.toggleSort = !this.toggleSort;
-    })
-  }
+  ngOnInit() {}
   
   getname(event)
   {
@@ -53,8 +38,10 @@ export class PlayerTableComponent implements OnInit {
       {
         this.index = this.alltitle.findIndex(element=> element == this.value)
         this.object = this.allvalue[this.index];
+        this.sortArray();
+        this.toggleSort = !this.toggleSort;
       }
-    });
+    }); 
   }
   
   sortArray () 
