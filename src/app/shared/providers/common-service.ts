de391a655b0723b560dfbb9d528b98b43dba1df3
Savan Rajkotiya <savan.r@yudiz.in>
2019-08-29 15:37:32 +0530

    ngrx effects configure for soccer daily fixtures

--- src/app/shared/providers/common-service.ts
+++ src/app/shared/providers/common-service.ts
@@ -136,10 +136,20 @@ export class CommonService {
       else if (type == 'team') {
         return 'sr:competitor:' + id
       }
-      else if(type == 'player'){
-        return 'sr:player:'+id
+      else if (type == 'player') {
+        return 'sr:player:' + id
       }
     }
   }
+
+  convertDate(date) {
+    var yyyy = date.getFullYear().toString();
+    var mm = (date.getMonth() + 1).toString();
+    var dd = date.getDate().toString();
+    var mmChars = mm.split('');
+    var ddChars = dd.split('');
+    return yyyy + '-' + (mmChars[1] ? mm : "0" + mmChars[0]) + '-' + (ddChars[1] ? dd : "0" + ddChars[0]);
+  }
+
 
 }
