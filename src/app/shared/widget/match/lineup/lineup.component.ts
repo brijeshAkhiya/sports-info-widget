9ee6faab35c3589bd2ac0ff868b2cde32b6beecf
Rajnee Yudiz <rajnee.m@yudiz.in>
2019-09-10 14:08:43 +0530

    Kabaddi and Soccer module RTL

--- src/app/shared/widget/match/lineup/lineup.component.ts
+++ src/app/shared/widget/match/lineup/lineup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 import { SportsService } from "@providers/sports-service";
 import { CricketService } from "@providers/cricket-service";
@@ -8,7 +8,8 @@ import { CommonService } from "@providers/common-service";
 @Component({
   selector: 'app-lineup',
   templateUrl: './lineup.component.html',
-  styleUrls: ['./lineup.component.css']
+  styleUrls: ['./lineup.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class LineupComponent implements OnInit {
 
