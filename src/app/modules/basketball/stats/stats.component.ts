46fad264a838025a17e45d290f1d6c92a029cf08
pooja <pooja.g@yudiz.in>
2019-09-30 11:17:57 +0530

    states filter changes

--- src/app/modules/basketball/stats/stats.component.ts
+++ src/app/modules/basketball/stats/stats.component.ts
@@ -12,6 +12,7 @@ export class StatsComponent implements OnInit {
   stats: any;
   isloading = false;
   filter: any = { 'category': 'points' };
+  namesdata: any;
   constructor(
     private sportsService: SportsService
   ) { }
@@ -26,11 +27,8 @@ export class StatsComponent implements OnInit {
     this.sportsService.getBasketballstats(this.filter.year, this.filter.type).subscribe((res: any) => {
       if (res) {
         this.isloading = false;
-        if (res.data.categories.length > 0) {
-          let stats = res.data.categories.filter((category) => category.name == this.filter.names && category.type == 'average');
-          if (stats.length > 0)
-            this.stats = stats[0].ranks;
-        }
+        this.namesdata = res.data.categories;
+        this.namesfilter();
         let name = [];
         res.data.categories.forEach(element => {
           /* // get unique seasons.type.code */
@@ -46,16 +44,19 @@ export class StatsComponent implements OnInit {
     this.sportsService.getBasketballseason().subscribe((res: any) => {
       if (res.data && res.data.seasons) {
         if (res.data.seasons) {
-          if (!localStorage.getItem('filteryear')) {
+          if (!localStorage.getItem('filteryear') || !localStorage.getItem('filtertype') || !localStorage.getItem('filternames')) {
             this.filter.year = res.data.seasons[res.data.seasons.length - 1].year;
             localStorage.setItem('filteryear', this.filter.year);
             this.filter.type = res.data.seasons[res.data.seasons.length - 1].type.code;
             localStorage.setItem('filtertype', this.filter.type);
+            this.filter.names = 'points';
+            localStorage.setItem('filternames', this.filter.names);
           } else {
             this.filter.year = localStorage.getItem('filteryear');
             this.filter.type = localStorage.getItem('filtertype');
+            this.filter.names = localStorage.getItem('filternames');
           }
-          this.filter.names = 'points';
+          // this.filter.names = 'points';
         }
 
         let years = [];
@@ -77,6 +78,13 @@ export class StatsComponent implements OnInit {
     return self.indexOf(value) === index;
   }
 
+  namesfilter() {
+    if (this.namesdata.length > 0) {
+      let stats = this.namesdata.filter((category) => category.name == this.filter.names && category.type == 'average');
+      if (stats.length > 0)
+        this.stats = stats[0].ranks;
+    }
+  }
   filterData(params) {
     if (params.names)
       this.filter.names = params.names;
@@ -84,11 +92,14 @@ export class StatsComponent implements OnInit {
       this.filter.year = params.year;
     if (params.type)
       this.filter.type = params.type;
-    if (params.type)
-      this.filter.category = params.category;
-    this.getTournamentStats();
+      if (!params.names) {
+        this.getTournamentStats();
+      } else {
+        this.namesfilter();
+      }
     localStorage.setItem('filteryear', this.filter.year);
     localStorage.setItem('filtertype', this.filter.type);
+    localStorage.setItem('filternames', this.filter.names);
   }
 
 }
