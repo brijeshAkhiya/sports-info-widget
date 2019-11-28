fe9fa3a7dbe1bda418085e082642fd36f8dfdfb3
Doli Vadiya <doli@yudiz.com>
2019-11-28 12:27:56 +0530

    Translation at server side

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -5,11 +5,30 @@ import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { Observable, Observer } from 'rxjs';
+import { readFileSync } from 'fs';
+
+export function universalLoader(): TranslateLoader {
+  return {
+    getTranslation: (lang: string) => {
+      return Observable.create((observer: Observer<any>) => {
+        observer.next(JSON.parse(readFileSync(`./dist/browser/assets/i18n_v2_5/${lang}.json`, 'utf8')));
+        observer.complete();
+      });
+    }
+  } as TranslateLoader;
+}
+
+
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
     ModuleMapLoaderModule,
+    TranslateModule.forRoot({
+      loader: { provide: TranslateLoader, useFactory: universalLoader }
+    })
   ],
   bootstrap: [AppComponent],
 })
