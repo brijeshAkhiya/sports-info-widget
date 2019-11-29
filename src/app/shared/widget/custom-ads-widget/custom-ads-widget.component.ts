1437d093fd5d992959a5f98a5faff075d9803358
Doli Vadiya <doli@yudiz.com>
2019-11-29 18:03:16 +0530

    Load Js for advertisement

--- src/app/shared/widget/custom-ads-widget/custom-ads-widget.component.ts
+++ src/app/shared/widget/custom-ads-widget/custom-ads-widget.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input, PLATFORM_ID, Inject } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { isPlatformBrowser } from '@angular/common';
+import { isPlatformBrowser, DOCUMENT } from '@angular/common';
 import { SportsService } from '@providers/sports-service';
 
 declare var window: any;
@@ -10,22 +10,23 @@ declare var window: any;
   styleUrls: ['./custom-ads-widget.component.css']
 })
 export class CustomAdsWidgetComponent implements OnInit {
-  @Input() type: any
+  @Input() type: any;
   adsObj = [];
   addata: any;
-  defaultImageLarge = '/assets/images/ad-1320-300.jpg'
-  defaultImageRectangle = '/assets/images/ad-320-267.jpg'
-  defaultImageBanner = '/assets/images/ad-320-80.jpg'
+  defaultImageLarge = '/assets/images/ad-1320-300.jpg';
+  defaultImageRectangle = '/assets/images/ad-320-267.jpg';
+  defaultImageBanner = '/assets/images/ad-320-80.jpg';
 
   constructor(
     private store: Store<any>,
     private sportsService: SportsService,
-    @Inject(PLATFORM_ID) private platformId: Object
+    @Inject(PLATFORM_ID) private platformId: Object,
+    @Inject(DOCUMENT) private _document: Document,
   ) { }
 
   ngOnInit() {
     if (isPlatformBrowser(this.platformId)) {
-      //ngrx store code
+      // ngrx store code
       this.store.select('ads').subscribe((data: any) => {
         let arr = data.Ads;
         arr.map(data => {
@@ -33,18 +34,21 @@ export class CustomAdsWidgetComponent implements OnInit {
             this.adsObj[data.eType] = [];
           this.adsObj[data.eType].push(data);
         });
-        //pick random custom ad from ngrx store data obj
-        if (this.adsObj[this.type])
+        // pick random custom ad from ngrx store data obj
+        if (this.adsObj[this.type]) {
           this.addata = this.adsObj[this.type][Math.floor(Math.random() * this.adsObj[this.type].length)];
-      })
+          if (this.addata.sJS) {
+            this.loadJS(this.addata.sJS);
+          }
+        }
+
+      });
     }
   }
 
   /** For load custom js */
-  loadJS() {
-    var myScript = document.createElement('script');
-    myScript.textContent = ''; // TEMP Load Js
-    document.head.appendChild(myScript);
+  loadJS(ads) {
+    this._document.body.append(this._document.createRange().createContextualFragment(ads))
   }
 
   adclick(id, adlink) {
@@ -54,10 +58,7 @@ export class CustomAdsWidgetComponent implements OnInit {
 
   updateAdclickCount(id) {
     if (id) {
-      this.sportsService.updateaddclickcount(id).subscribe((res) => {
-        if (res) {
-        }
-      })
+      this.sportsService.updateaddclickcount(id);
     }
   }
 }
